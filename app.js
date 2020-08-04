ef6a8910bba6c804d5327563aa0f84a6d8a8997d
Michael Mekuleyi <mekuleyimichael@gmail.com>
2020-08-05 00:06:38 +0100

    added put and patch

--- app.js
+++ app.js
@@ -3,43 +3,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Book = require("./models/bookModel");
 const bodyParser = require("body-parser");
+const bookRouter = require("./routes/bookRouter")(Book);
 
-const app = express();
-const bookRouter = express.Router();
 const db = mongoose.connect("mongodb://localhost/bookAPI");
 const port = process.env.PORT || 3000;
 
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-bookRouter
-  .route("/books")
-  .post((req, res) => {
-    const book = new Book(req.body);
-    book.save();
-    return res.status(201).json(book);
-  })
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    });
-  });
-
-bookRouter.route("/books/:bookId").get((req, res) => {
-  Book.findById(req.params.bookId, (err, book) => {
-    if (err) {
-      return res.send(err);
-    }
-    return res.json(book);
-  });
-});
 app.use("/api", bookRouter);
 
 app.get("/", (req, res) => {
